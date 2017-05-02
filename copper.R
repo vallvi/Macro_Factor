205e3111a8c1d1425b3691a3fde3435413e0f99b
Kevin Bonds <kevin.w.bonds@gmail.com>
2017-05-02 16:24:25 -0700

    cleanup code and add folding to copper.R

--- copper.R
+++ copper.R
@@ -9,30 +9,7 @@ library(tidyr)
 library(XML)
 library(RSelenium)
 
-# copper_login <- POST("https://www.metalprices.com/a/Login", authenticate("neha_shah@gap.com", "gapster6"))
-# copper_response <- GET("https://www.metalprices.com/feeds/shfe/copper", verbose(info = ))
-# 
-# rawToChar(copper_login$content)
-# 
-# stringi::stri_enc_detect(content(copper_response, "raw"))
-# 
-# copper <- rawToChar(content(copper_response, "raw"))
-# 
-# copper_read <- read_html(copper_response)
-# 
-# req <- curl_fetch_memory("https://www.metalprices.com/feeds/shfe/copper")
-# 
-# 
-# 
-# #### rvest
-# 
-# copper_html <- read_html("https://www.metalprices.com/feeds/shfe/copper")
-# copper_html %>% 
-#   html_node("#Username") %>% 
-#   html_text()
-
-#### RSelenium
-
+# Start PhantomJS server and log in to metals.com using RSelenium ----
 pJS <- phantom(pjs_cmd = "C:\\Users\\Ke2l8b1\\Documents\\Shipping_origin_map\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe")
 
 Sys.sleep(2)
@@ -48,10 +25,6 @@ Sys.sleep(8)
 remDr$getCurrentUrl()
 # remDr$refresh()
 
-
-#webElem <- remDr$findElement(using = "css", "input#Username.login.valid")
-
-#webElem <- remDr$findElement(using = "class name", "login")
 webElem <- remDr$findElement(using = "name", "Username")
 
 webElem$getElementAttribute("id")
@@ -66,19 +39,16 @@ webElem <- remDr$findElement(using = "name", "Password")
 webElem$sendKeysToElement(list("gapster6"))
 webElem$getElementAttribute("value")
 webElem$sendKeysToElement(list(key= "enter"))
-# webElem$getElementAttribute("value")
-# webElem <- remDr$findElement(using = "class", "input.Login")
-my_source <-  remDr$getPageSource
+
 webElem$screenshot(useView  = TRUE, display = TRUE)
 
 # copper ----
 remDr$navigate("https://www.metalprices.com/feeds/shfe/copper")
 tableElem <- remDr$findElement("css", "table")
-# tableElem$highlightElement()
 
 copper <-  readHTMLTable(tableElem$getElementAttribute("outerHTML")[[1]])
 View(copper)
-# clean up table
+# clean up copper table ----
 copper <- as.data.frame(copper[1])
 colnames(copper) <- as.character(unlist(copper[3,]))
 copper <- copper[-c(1:3), ]
@@ -90,7 +60,7 @@ tableElem <- remDr$findElement("css", "table")
 
 zinc <-  readHTMLTable(tableElem$getElementAttribute("outerHTML")[[1]])
 View(zinc)
-# clean up table
+# clean up zinc table ----
 zinc <- as.data.frame(zinc[1])
 colnames(zinc) <- as.character(unlist(zinc[3,]))
 zinc <- zinc[-c(1:3), ]
@@ -102,6 +72,7 @@ metal <- rbind(copper, zinc)
 # SwitchElem <- remDr$findElement("css", "input")
 # SwitchElem$clickElement()
 
+# EXP ----
 # remDr$navigate("https://www.metalprices.com/a/Logout")
 # webElem <- remDr$findElement(using = "css", "input")
 # webElem$clickElement()
